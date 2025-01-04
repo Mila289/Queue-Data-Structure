9f07532eb5bfaf5191aff896c8762a621a043d4f
Mila289 <mila.krasulina@gmail.com>
2025-01-04 12:22:02 +0700

    Добавляю условия.

--- Queue Data Structure.py
+++ Queue Data Structure.py
@@ -1,1 +1,22 @@
 class Queue:
+
+    def __init__(self):
+        self.items = []
+
+    def __repr__(self):
+        return f'Queue object: data={self.items}'
+
+    def is_empty(self):
+        return not self.items
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        return self.items.pop(0)
+
+    def size(self):
+        return len(self.items)
+
+    def peek(self):
+        return self.items[0]
